cb34df7bbd0df557c1aab992d290b5e7ecf9d442
andev <andh1006@naver.com>
2025-09-08 11:31:09 +0900

    Swift Attrubute

--- SeSAC7HardwareDatabase/Onboarding/OnboardingViewController.swift
+++ SeSAC7HardwareDatabase/Onboarding/OnboardingViewController.swift
@@ -37,6 +37,13 @@ class ThirdViewController: UIPageViewController {
 
 }
 
+// @unknown: 멤버가 추가될 가능성이 있는 열거형, unfrozen Enum (라이브러리, 프레임워크)
+enum Onboarding: Int {
+    case first = 0
+    case second
+    case third
+}
+
 final class OnboardingViewController: UIPageViewController {
     
     var list: [UIViewController] = []
@@ -52,7 +59,28 @@ final class OnboardingViewController: UIPageViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let a = NSTextAlignment.center
+        
+        switch a {
+        case .left:
+            <#code#>
+        case .center:
+            <#code#>
+        case .right:
+            <#code#>
+        case .justified:
+            <#code#>
+        case .natural:
+            <#code#>
+        @unknown default:
+            <#fatalError()#>
+        }
+        
+        getRandom()
+        
         list = [FirstViewController(), SecondViewController(), ThirdViewController()]
+        
+        view.backgroundColor = .systemBlue
         
         delegate = self
         dataSource = self
@@ -61,12 +89,30 @@ final class OnboardingViewController: UIPageViewController {
         setViewControllers([first], direction: .forward, animated: true)
     }
     
+    @discardableResult
+    func getRandom() -> Int {
+        let random = Int.random(in: 1...100)
+        print(random)
+        return random
+    }
+    
 
 
 }
 
 
 extension OnboardingViewController: UIPageViewControllerDelegate, UIPageViewControllerDataSource {
+    
+    func presentationCount(for pageViewController: UIPageViewController) -> Int {
+        return list.count
+    }
+    
+    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+        guard let first = viewControllers?.first, let index = list.firstIndex(of: first) else {
+            return 0
+        }
+        return index
+    }
     
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
         //현재 보고있는 뷰컨 배열의 인덱스
