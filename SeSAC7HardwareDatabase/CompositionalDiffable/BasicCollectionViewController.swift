b709a4c9cdfdbdd28dcecd26cac82b89d116ac24
andev <andh1006@naver.com>
2025-09-15 11:23:16 +0900

    CompositionalLayout list 연동

--- SeSAC7HardwareDatabase/CompositionalDiffable/BasicCollectionViewController.swift
+++ SeSAC7HardwareDatabase/CompositionalDiffable/BasicCollectionViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 import SnapKit
 
+struct Basic {
+    let name: String
+    let age: Int
+}
+
 class BasicCollectionViewController: UIViewController {
  
     private let searchBar = UISearchBar()
     private lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: createLayout())
  
-    var registration: UICollectionView.CellRegistration<UICollectionViewListCell, String>!
+    private var registration: UICollectionView.CellRegistration<UICollectionViewListCell, Basic>!
+    
+    let list = [
+        Basic(name: "Jack", age: 123),
+        Basic(name: "Den", age: 13),
+        Basic(name: "Finn", age: 1234),
+        Basic(name: "Bran", age: 11),
+        Basic(name: "Hue", age: 55)
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,10 +46,13 @@ class BasicCollectionViewController: UIViewController {
     }
     
     private func configureCellRegistration() {
+        print(#function)
         registration = UICollectionView.CellRegistration { cell, indexPath, itemIdentifier in
+            print("cell registration", indexPath)
             var content = UIListContentConfiguration.valueCell() // systemCell
-            content.text = itemIdentifier
+            content.text = itemIdentifier.name
             content.textProperties.color = .brown
+            content.secondaryText = "\(itemIdentifier.age)세"
             cell.contentConfiguration = content
             
             var background = UIBackgroundConfiguration.listGroupedCell()
@@ -48,14 +64,15 @@ class BasicCollectionViewController: UIViewController {
 
 extension BasicCollectionViewController: UICollectionViewDataSource, UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 100
+        return list.count
     }
     
     //1. Custom Cell + register + identifier
     //2. System Cell + Cell Resgistration + X
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        let cell = collectionView.dequeueConfiguredReusableCell(using: registration, for: indexPath, item: "고래밥")
+        print("cellForItemAt", indexPath)
+        let cell = collectionView.dequeueConfiguredReusableCell(using: registration, for: indexPath, item: list[indexPath.row])
         
         return cell
     }
